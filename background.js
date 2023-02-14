d5cfe18721307ee697c755de4eae40963db25f97
abderox <abdelhadi12mouzafir@gmail.com>
2023-02-15 00:35:54 +0100

    NG_NAds : avalanche commit v1.0.0.0

---
+++ background.js
@@ -0,0 +1,160 @@
+/**
+ * @author : https://github.com/abderox
+ * @Extension_Name : NG_NAds (No gain , no ads)
+ * @Version : 1.0.0
+ */
+
+
+try {
+
+    chrome.action.onClicked.addEventListner((tab) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['script.js'],
+        });
+    }
+    )
+
+} catch (error) {
+    console.log(error);
+}
+
+
+
+const serving_ad_domains = [
+    'doubleclick.net',
+    'googlesyndication.com',
+    'googleadservices.com',
+    'adnxs.com',
+    'adzerk.net',
+    'rubiconproject.com',
+    'openx.net',
+    'pubmatic.com',
+    'appnexus.com',
+    'criteo.com',
+    'taboola.com',
+    'outbrain.com',
+    'amazon-adsystem.com',
+    'connatix.com',
+    'distroscale.com',
+    'freestar.com',
+    'liveramp.com',
+    'mediavine.com',
+    'onetag.com',
+    'sharethrough.com',
+    'triplelift.com',
+    'yieldmo.com',
+    'yieldlab.net',
+    'zedo.com'
+];
+
+
+// ! old version 2
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details) {
+//       if (details.initiator === undefined || details.initiator === null) {
+//         // Block the request if it was not initiated by the user
+//         return { cancel: true };
+//       }
+
+//       // Check if the request is for an ad-serving domain
+//       var adDomainRegex = new RegExp('^https?://([^.]+\\.)?(' + serving_ad_domains.join('|') + ')/.*$');
+//       if (adDomainRegex.test(details.url)) {
+//         // Block the request if it matches an ad-serving domain
+//         return { cancel: true };
+//       }
+//     },
+//     {urls: ["<all_urls>"]},
+//     ["blocking"]
+//   ); 
+
+// function hashCode(str) {
+//     // A simple hash function to generate a unique identifier for each domain
+//     let hash = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         hash = ((hash << 5) - hash) + str.charCodeAt(i);
+//         hash |= 0;
+//     }
+//     return hash;
+// }
+
+String.prototype.hashCode = function () {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash;
+}
+
+
+// chrome.declarativeNetRequest.getDynamicRules(rules => {
+//     const existingDomains = new Set();
+//     for (const rule of rules) {
+//         const filter = rule.condition.urlFilter;
+//         const matches = filter.match(/^https?:\/\/([^/]+)/);
+//         if (matches) {
+//             existingDomains.add(matches[1]);
+//         }
+//     }
+
+//     const newRules = [];
+//     serving_ad_domains.forEach(domain => {
+//         if (!existingDomains.has(domain)) {
+//             const id = Math.floor(Math.random() * 1e9);
+//             const rule = {
+//                 id,
+//                 priority: Math.floor(Math.random() * 99999),
+//                 action: { type: 'block' },
+//                 condition: {
+//                     urlFilter: `*://${domain}/*`,
+//                     initiator: {
+//                         type: 'user'
+//                     }
+//                 }
+
+//             };
+//             newRules.push(rule);
+//         }
+//     });
+
+//     if (newRules.length > 0) {
+//         chrome.declarativeNetRequest.updateDynamicRules({ addRules: newRules }, () => {
+//             console.log('Rules updated.');
+//         });
+//     }
+// });
+
+
+    chrome.declarativeNetRequest.getDynamicRules( (rules) => {
+      const existingDomains = new Set();
+      for (const rule of rules) {
+        const filter = rule.condition.urlFilter;
+        const matches = filter.match(/^https?:\/\/([^/]+)/);
+        if (matches) {
+          existingDomains.add(matches[1]);
+        }
+      }
+  
+      const newRules = serving_ad_domains.filter(domain => !existingDomains.has(domain)).map((domain, index) => {
+        return {
+          id: Math.floor(Math.random() * 1e9),
+          priority: Math.floor(Math.random() * 99999),
+          action: { type: 'block' },
+          condition: {
+            urlFilter: `*://${domain}/*`,
+          }
+        };
+      });
+  
+      if (newRules.length > 0) {
+        chrome.declarativeNetRequest.updateDynamicRules({ addRules: newRules }, () => {
+          console.log('Rules added.');
+        });
+      }
+    });
+ 
---
+++ script.js
@@ -0,0 +1,125 @@
+/**
+ * @author : https://github.com/abderox
+ * @Extension_Name : NG_NAds (No gain , no ads)
+ * @Version : 1.0.0
+ */
+
+
+const query_select_all = (...array_of_strings) => {
+    var all = [];
+    array_of_strings.forEach(element => {
+        var temp = document.querySelectorAll(element);
+        all = all.concat(temp);
+    });
+    return all;
+}
+
+
+
+function start_removig_ads() {
+    'use strict';
+    query_select_all(
+        'div[class$="_ad',
+        'div[class^="ad_',
+        'div[class*="ad_',
+        'div[class*="_ad',
+        'div[class*="ad',
+        'div[class*="_ad_',
+        'iframe[class^="ad_',
+        'iframe[class$="_ad',
+        'iframe[class*="ad_',
+        'iframe[class*="_ad',
+        'iframe[class*="ad',
+        'iframe[class*="_ad',
+        'iframe[class*="ad_',
+        'iframe[class*="_ad_',
+        'iframe[src*="googleads',
+        'iframe[src*="doubleclick',
+        'iframe[src*="adnxs',
+        'div[class*="Sponsored',
+        'div[class*="sponsored',
+        'div[class*="Sponsor',
+        'div[class*="sponsor',
+        'div[class*="Sponsorship',
+        'div[class*="sponsorship',
+        'div[id*="_ad"]',
+        'div[id*="ad_"]',
+        'div[id*="ad"]',
+        'div[id*="_ad_"]',
+        'iframe[id*="_ad"]',
+        'iframe[id*="ad_"]',
+        'iframe[id*="ad"]',
+        'iframe[id*="_ad_"]',
+        'div[class*="ad-"]',
+        'div[id*="ad_"]',
+    ).forEach(element => {
+        if (element) {
+            element.forEach(item => {
+                if (item) {
+                    if (
+                        item.parentNode
+                    ) {
+                        item.parentNode.removeChild(item);
+                    }
+                    else {
+                        item.remove();
+                    }
+                }
+
+            });
+        }
+    });
+
+
+    // check if the link is youtube
+    if (window.location.href.includes('youtube')) {
+
+        query_select_all(
+            'ytd-compact-promoted-video-renderer',
+            'ytd-promoted-video-renderer',
+            'ytd-promoted-sparkles-web-renderer',
+            'ytd-promoted-sparkles-text-renderer'
+        ).forEach(element => {
+
+            if (element) {
+                element.forEach(item => {
+                    if (item) {
+                        if (
+                            item.parentNode
+                        ) {
+                            item.parentNode.removeChild(item);
+                        }
+                        else {
+                            item.remove();
+                        }
+                    }
+
+                });
+            }
+        });
+
+    }
+
+
+
+}
+
+start_removig_ads();
+
+timeout = null;
+document.addEventListener("DOMSubtreeModified", function () {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(function () {
+        start_removig_ads();
+    }, 2000);
+}, false);
+
+
+
+
+
+
+
+
